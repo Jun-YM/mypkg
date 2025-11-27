d2229438d334790696a4fd95908871b435e2ba8e
jun <0606jun4506@gmail.com>
2025-11-27 13:11:06 +0900

    talkerとlistenerを追加

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,17 +1,28 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
-
+from person_msgs.srv import Query
+from person_msgs.msg import Person
 rclpy.init()
 node = Node("listener")
-pub = node.create_publisher(Int16, "countup", 10)
-n = 0
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+def main():
+    client = node.create_client(Query, 'query') 
+    while not client.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('待機中')    
+    req = Query.Request()
+    req.name = "上田隆一"
+    future = client.call_async(req)
+    while rclpy.ok():
+        rclpy.spin_once(node) 
+        if future.done():
+            try:
+                response = future.result()
+            except:
+                node.get_logger().info('呼び出し失敗')
+            else:
+                node.get_logger().info("age: {}".format(response.age))
 
+            break
 
-def  main():
-    pub = node.create_subscription(Int16,"countup",cb,10)
-    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'mypkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jun0606',
     maintainer_email='s24C1133la@s.chibakoudai.jp',
     description='ロボットシステム学授業用',
-    license='BSD－3－Clause',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-             'talker = mypkg.talker:main', #talker.pyのmain関数という意味
+            'talker = mypkg.talker:main',
             'listener = mypkg.listener:main',
         ],
     },
