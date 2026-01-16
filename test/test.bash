fba6ec16115d9455a1db56c5ac814acf3aa4de1e
jun <0606jun4506@gmail.com>
2026-01-17 02:25:12 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -6,6 +6,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg wifi_monitor.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
+grep 'Published Wi-Fi signal: -44 dBm'
