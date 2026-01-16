e4710e863aa300df6ae7d0397a49cbda91a122ca
jun <0606jun4506@gmail.com>
2026-01-17 02:34:57 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -9,4 +9,4 @@ source $dir/.bashrc
 timeout 10 ros2 launch mypkg wifi_monitor.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep 'Published Wi-Fi signal: -44 dBm'
+grep 'Published Wi-Fi signal'
